d2d27d1eac7e8c23de7fc1e494a61f64b3b53043
Narendra R Giriyappanavar <naren.rg09@gmail.com>
2026-07-28 19:00:56 +0530

    js Practise

---
+++ JAVASCRIPT/practise-1.js
@@ -0,0 +1,14 @@
+
+// Syntax of declaring variables
+
+// let is used to declare a block-scoped variable whose value can be updated.
+let a = 5;
+let b = 6;
+
+// const keyword:
+// values of constants can't be changed with re-assignment & they can't be re-declared
+const c = 15;
+
+// Old Syntax of writing variables
+var v = 6;
+
