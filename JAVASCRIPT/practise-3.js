9fd83a29e00281f3d2cc4265046c0c044026bd87
Narendra R Giriyappanavar <naren.rg09@gmail.com>
2026-07-29 19:40:58 +0530

    js practise codes

---
+++ JAVASCRIPT/practise-3.js
@@ -0,0 +1,14 @@
+// console.log()
+// To write (log) a message on the console
+
+console.log("Hello World!");
+let num = 123;
+console.log(num);
+
+// Template Literals
+// They are used to add embedded expressions in a string.
+
+let a = 5;
+let b = 10;
+
+console.log(`Your pay ${a + b} rupees`);
