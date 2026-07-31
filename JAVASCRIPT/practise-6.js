886a65d50b31c87acd12d4f196eab795d3dc121b
Narendra R Giriyappanavar <naren.rg09@gmail.com>
2026-07-31 18:48:20 +0530

    [ractise js

---
+++ JAVASCRIPT/practise-6.js
@@ -0,0 +1,14 @@
+let student = {
+    name : "Alexios",
+    marks : 94.7,
+    age : 34,
+    city : london
+}
+
+student.name;
+student["name"]
+
+student.city = "Mumbai";
+student.gender = "Male";
+delete student.city;
+
