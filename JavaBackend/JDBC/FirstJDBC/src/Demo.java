fd038816886b2d72b54e0c03351a3fb09edb3d53
Narendra R Giriyappanavar <naren.rg09@gmail.com>
2026-08-03 11:05:18 +0530

    Driver Loaded

---
+++ JavaBackend/JDBC/FirstJDBC/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
