8aea2fd30dad31492d2a51436c924d9bc9858eb1
Narendra R Giriyappanavar <naren.rg09@gmail.com>
2026-08-11 22:23:47 +0530

    Backend:

---
+++ Node/Prac1/script.js
@@ -0,0 +1,22 @@
+// let n = 5;
+
+// for(let i =0; i<n;i++) {
+//     console.log("hello " + i);
+// }
+
+// let args = process.argv;
+
+// for(let i=2;i < args.length;i++) {
+//     console.log("Hello to " + args[i]);
+// }
+
+// const math = require("./math");
+
+// console.log(math.sum(2,2));
+// console.log(math.mul(2,2));
+// console.log(math.PI);
+// console.log(math.g);
+
+const info = require("./fruits");
+
+console.log(info);
